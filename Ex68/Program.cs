ec5b1966138709a095d0c002e1bba173bd99b960
Katya <katvas785@gmail.com>
2022-09-21 19:08:06 +0300

    Ex68

---
+++ Ex68/Program.cs
@@ -0,0 +1,16 @@
+﻿static uint Akk(uint n, uint m)
+{
+    
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+uint m = 3;
+uint n = 2;
+Console.WriteLine(Akk(m, n)); // 29
+
+ 
