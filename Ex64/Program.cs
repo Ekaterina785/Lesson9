a5ac2867dd7fb5a27f0b52efec94fc90e7afae7d
Katya <katvas785@gmail.com>
2022-09-21 19:06:19 +0300

    Ex64

---
+++ Ex64/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if(M<N)
+{
+    for(int m =M; m<=N;m++)
+    Console.WriteLine(m);
+}
+else
+{
+    for(int n =N; n<=M;n++ )
+    {
+        Console.WriteLine(n);
+    }
+}
